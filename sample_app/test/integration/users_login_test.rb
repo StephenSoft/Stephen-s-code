0715b525816f97f923cff090fa4a795303a30d63
Stephenzhao <Stephenzhao94@163.com>
2017-09-04 16:28:01 +0800

    Implement advanced login

--- sample_app/test/integration/users_login_test.rb
+++ sample_app/test/integration/users_login_test.rb
@@ -21,9 +21,25 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
  		delete logout_path
  		assert_not is_logged_in?
  		assert_redirected_to root_url
+ 		#模擬用戶在另一個窗口中點擊退出鏈接
+ 		delete logout_path
  		follow_redirect!
  		assert_select "a[href=?]",login_path
  		assert_select "a[href=?]",logout_path,      count: 0
  		assert_select "a[href=?]",user_path(@user), count: 0
   end 
+  
+  test "login with remembering" do
+  	log_in_as(@user, remember_me: '1')
+  	assert_not_empty cookies['remember_token']
+  end
+  
+  test "login without remembering" do
+		#登錄， 設定 cookie
+		log_in_as(@user, remember_me: '1')
+		delete logout_path
+		#再次登錄， 確認 cookie 被刪除了
+		log_in_as(@user, remember_me: '0')
+		assert_empty cookies['remember_token']
+  end
 end
